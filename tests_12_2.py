d2eb2d280db8417665dfecf168bdfbbc6927618c
vxndtt <alenakad16@mail.ru>
2024-12-11 15:01:24 +0500

    Update tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -8,7 +8,7 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.all_results = {}
+        cls.all_results = []
 
     def setUp(self):
         self.runner1 = Runner('Усэйн', 10)
@@ -17,25 +17,26 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        pprint(cls.all_results)
+        for item in cls.all_results:
+            print(item, end = '}\n')
 
     def test_run1(self):
-        t1 = Tournament(90, (self.runner1, self.runner3))
+        t1 = Tournament(90, self.runner1, self.runner3)
         results = t1.start()
         TournamentTest.all_results.append(results)
         self.assertTrue(results[2] == 'Ник')
 
     def test_run2(self):
-        t2 = Tournament(90, (self.runner2, self.runner3))
+        t2 = Tournament(90, self.runner2, self.runner3)
         results = t2.start()
         TournamentTest.all_results.append(results)
         self.assertTrue(results[2] == 'Ник')
 
     def test_run3(self):
-        t3 = Tournament(90, (self.runner1, self.runner2, self.runner3))
+        t3 = Tournament(90, self.runner1, self.runner2, self.runner3)
         results = t3.start()
         TournamentTest.all_results.append(results)
-        self.assertTrue(results[2] == 'Ник')
+        self.assertTrue(results[3] == 'Ник')
 
 
 if __name__ == '__main__':
