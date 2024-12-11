dc74ca9a93047edadc73240c3c7cd91c38de0a9a
vxndtt <alenakad16@mail.ru>
2024-12-11 14:48:10 +0500

    Update tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -5,9 +5,10 @@ from runner_and_tournament import *
 
 class TournamentTest(unittest.TestCase):
 
+
     @classmethod
     def setUpClass(cls):
-        all_results = {}
+        cls.all_results = {}
 
     def setUp(self):
         self.runner1 = Runner('Усэйн', 10)
@@ -16,22 +17,25 @@ class TournamentTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        pprint(all_results)
+        pprint(cls.all_results)
 
     def test_run1(self):
         t1 = Tournament(90, (self.runner1, self.runner3))
-        t1.start()
-        self.assertTrue(self.all_results[-1], 'Ник')
+        results = t1.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[2] == 'Ник')
 
     def test_run2(self):
-        t1 = Tournament(90, (self.runner2, self.runner3))
-        t1.start()
-        self.assertTrue(self.all_results[-1], 'Ник')
+        t2 = Tournament(90, (self.runner2, self.runner3))
+        results = t2.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[2] == 'Ник')
 
     def test_run3(self):
-        t1 = Tournament(90, (self.runner1, self.runner2, self.runner3))
-        t1.start()
-        self.assertTrue(self.all_results[-1], 'Ник')
+        t3 = Tournament(90, (self.runner1, self.runner2, self.runner3))
+        results = t3.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[2] == 'Ник')
 
 
 if __name__ == '__main__':
